0463639fef8971c88be244f445f8756a171185dd
Pouriya Kordaki <pouriya.kordaki@justeattakeaway.com>
2025-12-08 03:31:14 +0100

    add dynamic error

--- src/components/common/ErrorContent.vue
+++ src/components/common/ErrorContent.vue
@@ -1,1 +1,12 @@
-<template>Error!</template>
+<script setup lang="ts">
+defineProps<{
+  error: string
+}>();
+</script>
+<template>
+  <section>
+    <h1>Error</h1>
+    <br />
+    <h2>{{ error }}</h2>
+  </section>
+</template>
