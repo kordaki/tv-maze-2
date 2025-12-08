2153aa62838131c0c9f7938a17c38020cb5b52d5
Pouriya Kordaki <pouriya.kordaki@justeattakeaway.com>
2025-12-08 04:07:58 +0100

    add title to the pages

--- src/router/index.ts
+++ src/router/index.ts
@@ -8,13 +8,23 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: HomeView,
+      meta: {
+        title: 'Home',
+      },
     },
     {
       path: '/video/:id',
       name: 'video',
       component: () => import('../views/VideoView.vue'),
+      meta: {
+        title: 'Video Details',
+      },
     },
   ],
+})
+
+router.beforeEach((to) => {
+  document.title = to.meta?.title ?? 'Default Title'
 })
 
 export default router
